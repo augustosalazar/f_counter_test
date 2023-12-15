7fe80bca5bdeac95877c0677c15600b5d3dd83c4
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2023-12-15 10:45:56 -0500

    Clean integration test

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,3 +1,6 @@
+import 'package:f_counter_test/data/repositories/counter_repository.dart';
+import 'package:f_counter_test/domain/repositories/i_counter_repository.dart';
+import 'package:f_counter_test/domain/use_cases/counter_use_case.dart';
 import 'package:f_counter_test/ui/controllers/counter_controller.dart';
 import 'package:f_counter_test/ui/my_app.dart';
 import 'package:flutter/material.dart';
@@ -8,7 +11,13 @@ import 'package:integration_test/integration_test.dart';
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
+  setUp(() {
+    //Get.put(CounterController());
+  });
+
   testWidgets('Counter increments', (WidgetTester tester) async {
+    Get.put<ICounterRepository>(CounterRepository());
+    Get.put(CounterUseCase(Get.find()));
     Get.put(CounterController());
     await tester.pumpWidget(const MyApp());
 
@@ -30,53 +39,64 @@ void main() {
   });
 
   testWidgets('Counter increments with keys', (WidgetTester tester) async {
+    Get.put<ICounterRepository>(CounterRepository());
+    Get.put(CounterUseCase(Get.find()));
     Get.put(CounterController());
     await tester.pumpWidget(const MyApp());
 
 // Verify that our counter starts at 0.
-    final counterText =
-        find.byKey(const Key('counterText')).evaluate().single.widget as Text;
-    expect(counterText.data, "0");
+    expect(
+        (find.byKey(const Key('counterText')).evaluate().single.widget as Text)
+            .data,
+        "0");
 
     // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
-    final counterText2 =
-        find.byKey(const Key('counterText')).evaluate().single.widget as Text;
-    expect(counterText2.data, "1");
+    expect(
+        (find.byKey(const Key('counterText')).evaluate().single.widget as Text)
+            .data,
+        "1");
 
     Get.deleteAll();
   });
 
   testWidgets('Counter set value', (WidgetTester tester) async {
+    Get.put<ICounterRepository>(CounterRepository());
+    Get.put(CounterUseCase(Get.find()));
     Get.put(CounterController());
     await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
-    final counterText =
-        find.byKey(const Key('counterText')).evaluate().single.widget as Text;
-    expect(counterText.data, "0");
+    expect(
+        (find.byKey(const Key('counterText')).evaluate().single.widget as Text)
+            .data,
+        "0");
 
     await tester.enterText(find.byKey(const Key('counterTextField')), "4");
 
     await tester.tap(find.byKey(const Key('counterSetButton')));
     await tester.pump();
 
-    final counterText2 =
-        find.byKey(const Key('counterText')).evaluate().single.widget as Text;
-    expect(counterText2.data, "4");
+    expect(
+        (find.byKey(const Key('counterText')).evaluate().single.widget as Text)
+            .data,
+        "4");
 
     Get.deleteAll();
   });
 
   testWidgets('Counter reset value', (WidgetTester tester) async {
+    Get.put<ICounterRepository>(CounterRepository());
+    Get.put(CounterUseCase(Get.find()));
     Get.put(CounterController());
     await tester.pumpWidget(const MyApp());
     // Verify that our counter starts at 0.
-    final counterText =
-        find.byKey(const Key('counterText')).evaluate().single.widget as Text;
-    expect(counterText.data, "0");
+    expect(
+        (find.byKey(const Key('counterText')).evaluate().single.widget as Text)
+            .data,
+        "0");
 
     await tester.enterText(find.byKey(const Key('counterTextField')), "4");
 
@@ -86,9 +106,10 @@ void main() {
     await tester.tap(find.byKey(const Key('counterResetButton')));
     await tester.pump();
 
-    final counterText2 =
-        find.byKey(const Key('counterText')).evaluate().single.widget as Text;
-    expect(counterText2.data, "0");
+    expect(
+        (find.byKey(const Key('counterText')).evaluate().single.widget as Text)
+            .data,
+        "0");
     Get.deleteAll();
   });
 }
